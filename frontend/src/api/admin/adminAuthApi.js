8e17ab72b39e70ab1f91e82f35094f3f4133b290
abdelrhmannabildev-dev <abdelrhmannabil.dev@gmail.com>
2026-07-20 20:03:44 +0300

    fixed adminLogin url

--- frontend/src/api/admin/adminAuthApi.js
+++ frontend/src/api/admin/adminAuthApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const API_URL = import.meta.env.VITE_API_URL;
 
 export const adminLogin = async (username, password) => {
-    const response = await axios.post(`${API_URL}/login`, {
+    const response = await axios.post(`${API_URL}/auth/login`, {
         username,
         password,
     });
